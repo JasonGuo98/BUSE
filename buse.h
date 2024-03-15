d3a2a6c91f71ab015b53d4cea1fa0e39746c6693
jasonxjguo <jasonxjguo@tencent.com>
2024-03-15 21:13:34 +0800

    add async_buse_operations

--- buse.h
+++ buse.h
@@ -5,6 +5,9 @@
 extern "C" {
 #endif
 
+typedef void (*reply_callback)(int, int, int, const char *);
+
+
 #include <sys/types.h>
 
   struct buse_operations {
@@ -20,7 +23,21 @@ extern "C" {
     u_int64_t size_blocks;
   };
 
+  struct async_buse_operations {
+    int (*async_read)(void *buf, u_int32_t len, u_int64_t offset, reply_callback callback);
+    int (*async_write)(const void *buf, u_int32_t len, u_int64_t offset, reply_callback callback);
+    void (*async_disc)(reply_callback callback);
+    int (*async_flush)(reply_callback callback);
+    int (*async_trim)(u_int64_t from, u_int32_t len, reply_callback callback);
+
+    // either set size, OR set both blksize and size_blocks
+    u_int64_t size;
+    u_int32_t blksize;
+    u_int64_t size_blocks;
+  };
+  
   int buse_main(const char* dev_file, const struct buse_operations *bop, void *userdata);
+  int async_buse_main(const char* dev_file, const struct buse_operations *bop, void *userdata);
 
 #ifdef __cplusplus
 }
